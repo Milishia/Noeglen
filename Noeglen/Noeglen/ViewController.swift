fe69a1e25a6e19e344c49eda6f914d27d128050f
Milishia Moradi <s175193@student.dtu.dk>
2019-10-03 09:40:08 +0200

    Tilføjet knapper med ikoner på

--- Noeglen/Noeglen/ViewController.swift
+++ Noeglen/Noeglen/ViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func dagbogButtonPressed(_ sender: Any) {
+        
+        
+        
+        
+       }
+     
 
 
 }
