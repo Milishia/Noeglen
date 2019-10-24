1ef94c81ee27a00840c9186e5a294cdce0aaec06
Milishia Moradi <s175193@student.dtu.dk>
2019-10-24 10:29:38 +0200

    Opdateret/Regeneret VideoforløbView med youtube videoer

---
+++ Noeglen/Noeglen/Controllers/OevelserViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OevelserViewController.swift
+//  Noeglen
+//
+//  Created by Milishia Moradi on 24/10/2019.
+//  Copyright © 2019 Caroline Ankjær Andersen. All rights reserved.
+//
+
+import UIKit
+
+class OevelserViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var videoArray = ["bla", "bla2","bla3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
