e0448240dc8ebd4e690f765d843a92e3c5aa6c94
caro <carlankjaer@gmail.com>
2019-11-30 14:59:42 +0100

    Tilføjede tekst om dagens humør i dagbog

--- Noeglen/Noeglen/Controllers/Diary/AddDiaryController.swift
+++ Noeglen/Noeglen/Controllers/Diary/AddDiaryController.swift
@@ -37,7 +37,7 @@ class AddDiaryController: UIViewController {
         print(savedMoodValueText)
         let diaryDate = "\(calendar.component(.day, from: date))/\(calendar.component(.month, from: date))/\(calendar.component(.year, from: date))"
         if let addDiaryTitle = addDiaryTitleTextView.text, let addDiaryDescription = addDiaryDescriptionTextView.text {
-            diaryService.addToList(diaryTitle: addDiaryTitle, diaryDescription: addDiaryDescription, diaryDate: diaryDate, diaryMood: savedMoodValueText, completion: { (status) in
+            diaryService.addToList(diaryTitle: addDiaryTitle, diaryDescription: "\(addDiaryDescription) \n\nMit humør idag var: \(savedMoodValueText)", diaryDate: diaryDate, diaryMood: savedMoodValueText, completion: { (status) in
                 if (status) {
                     print("Status", status)
                 }
